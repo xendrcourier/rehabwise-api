3add3b8a99116749c5c8d08390001bbbe49e11ac
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2026-06-09 00:07:21 +0100

    refactor:the prisma

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { AuthUtilService } from './auth.utils';
+import { PrismaService } from 'src/global/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly authUtilService: AuthUtilService,
+    private readonly prismaClient: PrismaService,
+  ) {}
+}
