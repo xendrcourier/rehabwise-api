62c954564fa54c222be36f9e86c9ba5e674cc634
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2026-06-10 20:57:56 +0100

    feat:added the exercise modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ExerciseModule } from './exercise/exercise.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ExerciseModule],
   controllers: [AppController],
   providers: [AppService],
 })
