2c6f5d5c751c167585a85767de6d919ff26c9895
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2026-07-08 04:57:46 +0100

    feat(exercise): add MulterModule for memory storage in ExerciseModule

--- src/exercise/exercise.module.ts
+++ src/exercise/exercise.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
 import { ExerciseController } from './exercise.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { memoryStorage, memoryStorage as multerMemoryStorage } from 'multer';
 
 @Module({
   controllers: [ExerciseController],
   providers: [ExerciseService],
   exports: [ExerciseService],
+  imports: [MulterModule.register({ storage: memoryStorage() })],
 })
 export class ExerciseModule {}
