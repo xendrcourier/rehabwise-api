c276c2e069bdf6c81f55d8558afa84b25a08e8de
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2026-07-22 07:03:30 +0100

    feat: add VideoCallModule to TherapistModule imports

--- src/therapist/therapist.module.ts
+++ src/therapist/therapist.module.ts
@@ -4,10 +4,11 @@ import { TherapistController } from './therapist.controller';
 import { PrismaModule } from 'src/global/prisma/prisma.module';
 import { ExerciseModule } from '../exercise/exercise.module';
 import { ProgramModule } from '../program/program.module';
+import { VideoCallModule } from 'src/integrations/video-call/video-call.module';
 
 @Module({
   controllers: [TherapistController],
   providers: [TherapistService],
-  imports: [PrismaModule, ExerciseModule, ProgramModule],
+  imports: [PrismaModule, ExerciseModule, ProgramModule, VideoCallModule],
 })
 export class TherapistModule {}
